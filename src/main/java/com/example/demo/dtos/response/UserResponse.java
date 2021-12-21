9371a9d03d40d96aeace35bfd09fcaa70315ddec
giangnt <hjhjminh0598@gmail.com>
2021-12-21 13:45:36 +0700

    dev com

--- src/main/java/com/example/demo/dtos/response/UserResponse.java
+++ src/main/java/com/example/demo/dtos/response/UserResponse.java
@@ -9,7 +9,9 @@ public class UserResponse {
     private String id;
     private String username;
     private String email;
+    private String companyId;
     private String companyName;
     private Long createAt;
+    private Long updateAt;
     private Boolean isDeleted;
 }
